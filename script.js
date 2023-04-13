9851609c66ca5de33790bd55c978c24e60b06652
SwapnilYC <119727525+SwapnilYC@users.noreply.github.com>
2023-04-13 18:08:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
 //your JS code here. If required.
-document.addEventListener('DOMContentLoaded', ()=>{
-	document.write = `DOM load success`;
-})
-// const body = document.getElementById("mainBody");
-// let html = "<p>DOM load success</p>";
-// body.insertAdjacentHTML("afterbegin", html);
